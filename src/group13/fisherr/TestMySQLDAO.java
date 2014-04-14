c18caad2c110ecb5969196884b2380e4ce3850ac
unknown <lemkedj@LAB-P107-07.labs.uwec.edu>
2014-04-14 15:32:12 -0500

    Finished tests

--- src/group13/fisherr/TestMySQLDAO.java
+++ src/group13/fisherr/TestMySQLDAO.java
@@ -20,40 +20,75 @@ import junit.framework.TestCase;
 
 public class TestMySQLDAO extends TestCase {
 	
-//	public void TestConnect(){
-//		MySQLDAO SQL = new MySQLDAO();
-//		SQL.connect();
+	
+	//This is the same as Execute, but in order to select something we must be connected
+	//so if this runs, then connect works.
+	public void testConnect(){
+	MySQLDAO SQL = new MySQLDAO();
+	SQL.connect();
+	
+	
+	String query = "SELECT * FROM Vendor;";
+	int result = 2;
+	result = SQL.execute(query);
+//	System.out.println(result);
+	assertNotSame(result, 1);
+	SQL.disconnect();
 //		
-//	}
-//	public void TestUpdate(){
-//		
-//		
-//	}
-	public void TestExecute(){
+	}
+
+	//Executes a select query
+	public void testExecute(){
 		MySQLDAO SQL = new MySQLDAO();
 		SQL.connect();
 		
-
-		String vendorName = "TestMart";
-		String query = "INSERT INTO Vendor (VendorName) Values(\""+vendorName+"\")";
+		
+		String query = "SELECT * FROM Vendor;";
 		int result = 2;
 		result = SQL.execute(query);
-		System.out.println(result);
-		assertEquals(result, 0);
+//		System.out.println(result);
+		assertNotSame(result, 1);
 		SQL.disconnect();
 	}
-//	public void TestDisconnect(){
-//		MySQLDAO SQL = new MySQLDAO();
-//		SQL.connect();
-//		SQL.disconnect();
-//
-//		String vendorName = "TestMart";
-//		String query = "INSERT INTO Vendor (VendorName) Values(\""+vendorName+"\")";
-//		int result = 2;
-//		result = SQL.execute(query);
-//		System.out.println(result);
-//		assertEquals(result, 1);
-//	}
+	/*
+	public void testUpdate(){
+	MySQLDAO SQL = new MySQLDAO();
+	SQL.connect();
+	
+	String vendorName = "TestMart";
+	String query = "INSERT INTO Vendor (VendorName) Values(\""+vendorName+"\")";
+	int result = 2;
+	result = SQL.executeUpdate(query);
+	System.out.println(result);
+	assertNotSame(result, 0);
+	SQL.disconnect();
+}
+*/
+	
+	public void testDisconnect(){
+		MySQLDAO SQL = new MySQLDAO();
+		SQL.connect();
+		SQL.disconnect();
+		
+		String query = "SELECT * FROM Vendor;";
+		int result = 2;
+		result = SQL.execute(query);
+		System.out.println("Result" + result);
+		//Not connected to database so should give an error, which makes result = 1
+		assertEquals(result, 1);
+		
+
+		
+		SQL.connect();
+		int result2 = 2;
+		result2 = SQL.execute(query);
+		System.out.println("Result" + result2);
+		//Same thing but now disconnect is after, so it should not be 1.
+		assertFalse(result2 == 1);
+		SQL.disconnect();
+		
+			
+	}
 	
 
 }
